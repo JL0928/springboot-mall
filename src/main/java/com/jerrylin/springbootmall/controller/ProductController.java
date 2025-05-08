e585b3189641173da212e04ebff4e65e5820f6a6
JL020928 <linpoting@exentric.com.tw>
2025-05-08 17:54:50 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/jerrylin/springbootmall/controller/ProductController.java
+++ src/main/java/com/jerrylin/springbootmall/controller/ProductController.java
@@ -54,4 +54,10 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
 
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable int productId){
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
